f9f8f5a1079cab56fd394b5961a3240bc9b51d50
Mo Mesgin <mo.mesgin@gmail.com>
2018-02-02 14:27:48 -0800

    finish

---
+++ spoiler.js
@@ -0,0 +1,46 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const readlineSync = require('readline-sync');
+
+console.log(`\nWelcome, I'm a Movie Spoiler Robot\n`)
+
+let movieTitle = readlineSync.question('Which movie do you want me to spoil for you? ')
+let spoilerTime = readlineSync.question('In how many seconds do you want me to do this? ')
+
+while (spoilerTime < 0) {
+    console.log('Time should be a positive number')
+    spoilerTime = readlineSync.question('In how many seconds do you want me to do this? ')
+}
+
+let googleSearchUrl = `https://www.google.ca/search?q=${movieTitle}+movie`
+
+request(googleSearchUrl, function (err, response, body) {
+    if (err) console.log(err)
+
+    let $ = cheerio.load(body)
+    let googleResults = [];
+
+    $('#res h3.r').each(function (i, elem) {
+        console.log($(this).text())
+        googleResults[i] = $(this).text();
+    });
+
+    request(`https://api.themoviedb.org/3/search/movie?api_key=d730ddf7f4c9229a1877979dcfa57302&query=${movieTitle}`, function (err, response, data) {
+        const dataObject = JSON.parse(data)
+
+        if (dataObject.results.length === 0) {
+            console.log(`\nThere is no movie with this title: ${movieTitle}\n`)
+        } else {
+            console.log(`\n**Spoiler Warning** about to spoil the movie "${movieTitle.toUpperCase()}" in ${spoilerTime} seconds`)
+            
+            let messageBeforeSpoil = googleResults.join('\n');
+            
+            console.log(`\n${messageBeforeSpoil}`)
+
+            setTimeout(function () {
+                console.log(`\nSpoiler : \n ${dataObject.results[0].overview}\n`)
+            }, spoilerTime * 1000)
+        }
+    })
+
+})
